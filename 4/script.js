839ee9fd87a7ecd6c7715c467dd0e382a5ff7c6f
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2025-01-07 13:32:31 +0100

    Add click event listeners to all the 5 buttons

--- 4/script.js
+++ 4/script.js
@@ -8,3 +8,20 @@ buttonContainer.addEventListener("click", (event) => {
 buttonChildContainer.addEventListener("click", (event) => {
   alert("Child container clicked!");
 });
+
+//DOM Level 0 Event Handlers on Button 2
+const button2 = document.getElementById("button2");
+button2.onclick = function(){
+  alert("Button2 is clicked!")
+};
+
+//DOM Level 2 Event Listeners on Button 3
+const button3 = document.getElementById("button3");
+button3.addEventListener("click",function(event){
+  alert("Button3 is clicked!")
+});
+
+//Event Delegation on Button 4 and 5
+buttonChildContainer.addEventListener("click",function(event){
+  alert(`${event.target.textContent} is clicked!`);
+});
