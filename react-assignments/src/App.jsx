eed0fa94f18824a02ce6f8174ede4d899af4122b
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2025-01-16 12:01:18 +0100

    Finish the todo for assignment8

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <header>
         <h1>This is React</h1>
       </header>
-      <ChildComponent props = {person} />
+      <ChildComponent person = {person} />
       {
       /* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
     </div>
--- react-assignments/src/ChildComponent.jsx
+++ react-assignments/src/ChildComponent.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-function ChildComponent({props}){
-    const hobbiesArr = props.info.hobbies;
+function ChildComponent({person}){
+    const hobbiesArr = person.info.hobbies;
 
     return(
     <div>
-        <h2>Name:{props.name}</h2>
-        <h2>Age:{props.info.age}</h2>
-        <h2>City:{props.info.city}</h2>
-        <h2>Job:{props.info.job}</h2>
+        <h2>Name:{person.name}</h2>
+        <h2>Age:{person.info.age}</h2>
+        <h2>City:{person.info.city}</h2>
+        <h2>Job:{person.info.job}</h2>
         <h2>Hobbies:{hobbiesArr.map((hobby)=> hobby +" ")}</h2>
     </div>)
 }
